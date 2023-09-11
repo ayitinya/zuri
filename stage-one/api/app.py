ac0485037a99c69e6596dc92f22bd30e268fdee0
Rudy Ayitinya <ayitinya@outlook.com>
2023-09-11 21:10:10 +0000

    complete stage-one

---
+++ stage-one/api/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/endpoint', methods=['GET'])
+def get_endpoint():
+    slack_name = request.args.get('slack_name', 'Rudy Sulley')
+    track = request.args.get('track', 'backend')
+    github_file_url = request.args.get('github_file_url', '')
+    github_repo_url = request.args.get('github_repo_url', '')
+
+    # Get the current day of the week
+    current_day = datetime.utcnow().strftime('%A')
+
+    # Get the current UTC time in the desired format
+    utc_time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    response = {
+        "slack_name": slack_name,
+        "current_day": current_day,
+        "track": track,
+        "utc_time": utc_time,
+        "github_file_url": github_file_url,
+        "github_repo_url": github_repo_url,
+        "status_code": 200
+    }
+
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
